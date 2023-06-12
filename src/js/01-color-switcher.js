94b45e20315a40293782e94864026d8bf7efe496
BanderaGaming <Glory.To.Ukraine.1648@gmail.com>
2023-06-12 13:49:59 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+
+let intervalId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
+
+const colorChange = () => {
+    intervalId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+};
+
+const stopChange = () => {
+    stopBtn.disabled = true;
+    clearInterval(intervalId);
+    startBtn.disabled = false;
+}
+startBtn.addEventListener('click', colorChange);
+stopBtn.addEventListener('click', stopChange);
+stopBtn.disabled = true;
 
